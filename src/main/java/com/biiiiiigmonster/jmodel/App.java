bb6d44917d97793511c39f2437aa0910d5f9c79c
陆云峰 <v-luyunfeng@xiaomi.com>
2023-11-21 16:14:01 +0800

    feat(init): wip

---
+++ src/main/java/com/biiiiiigmonster/jmodel/App.java
@@ -0,0 +1,13 @@
+package com.biiiiiigmonster.jmodel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
