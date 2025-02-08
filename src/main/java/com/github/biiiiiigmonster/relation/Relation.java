27fe76a5ae8f4402f718288329cbc65360029d70
luyunfeng <biiiiiigmonster@gmail.com>
2025-02-08 21:58:45 +0800

    feat: WIP

--- src/main/java/com/github/biiiiiigmonster/relation/Relation.java
+++ src/main/java/com/github/biiiiiigmonster/relation/Relation.java
@@ -1,22 +1,49 @@
 package com.github.biiiiiigmonster.relation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import cn.hutool.core.util.ReflectUtil;
+import com.github.biiiiiigmonster.Model;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
-/**
- * <p>
- *
- * </p>
- *
- * @author v-luyunfeng
- * @date 2023/10/10 16:46
- */
-@Target(ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Relation {
-    String foreignKey() default "id";
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-    String localKey() default "id";
+@Getter
+@Slf4j
+public abstract class Relation {
+    protected Field relatedField;
+
+    public Relation(Field relatedField) {
+        this.relatedField = relatedField;
+    }
+
+    public abstract <T extends Model<?>, R extends Model<?>> List<R> getEager(List<T> models);
+
+    public abstract <T extends Model<?>, R extends Model<?>> void match(List<T> models, List<R> results);
+
+    protected <R extends Model<?>> List<R> byRelatedMethod(List<?> localKeyValueList, Map<Object, Method> relatedMethod) {
+        Object bean = relatedMethod.keySet().iterator().next();
+        Method method = relatedMethod.values().iterator().next();
+        if (List.class.isAssignableFrom(method.getParameterTypes()[0])) {
+            return ReflectUtil.invoke(bean, method, localKeyValueList);
+        } else {
+            log.warn("{}存在N + 1查询隐患，建议{}实现List参数的仓库方法", bean.getClass().getName(), method.getName());
+            return localKeyValueList.stream()
+                    .map(param -> ReflectUtil.invoke(bean, method, param))
+                    .filter(Objects::nonNull)
+                    .flatMap(r -> {
+                        if (List.class.isAssignableFrom(method.getReturnType())) {
+                            return ((List<R>) r).stream();
+                        } else {
+                            return Stream.of((R) r);
+                        }
+                    })
+                    .collect(Collectors.toList());
+        }
+    }
 }
