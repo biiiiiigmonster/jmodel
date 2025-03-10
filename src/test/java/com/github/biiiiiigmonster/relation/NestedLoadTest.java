0968c53c76fd81f5867f0c504880dca9793bc1e1
luyunfeng <biiiiiigmonster@gmail.com>
2025-03-10 15:40:44 +0800

    feat: WIP

--- src/test/java/com/github/biiiiiigmonster/relation/NestedLoadTest.java
+++ src/test/java/com/github/biiiiiigmonster/relation/NestedLoadTest.java
@@ -57,45 +57,4 @@ public class NestedLoadTest extends BaseTest {
         assertEquals("Docker", tags3.get(0).getName());
         assertEquals("Cloud", tags3.get(1).getName());
     }
-
-    @Test
-    public void shouldLoadPostsWithComments() {
-        // 使用selectBatchIds获取用户列表
-        List<User> userList = userMapper.selectBatchIds(Arrays.asList(1L, 2L));
-        assertEquals(2, userList.size());
-
-        // 使用RelationUtils.load加载嵌套关联数据
-        RelationUtils.load(userList, "posts.comments");
-
-        // 验证第一个用户的文章评论
-        User user1 = userList.get(0);
-        List<Post> posts1 = user1.getPosts();
-        assertNotNull(posts1);
-        assertEquals(2, posts1.size());
-
-        Post post1 = posts1.get(0);
-        List<Comment> comments1 = post1.getComments();
-        assertNotNull(comments1);
-        assertEquals(2, comments1.size());
-        assertEquals("Great tutorial!", comments1.get(0).getContent());
-        assertEquals("Very helpful content", comments1.get(1).getContent());
-
-        Post post2 = posts1.get(1);
-        List<Comment> comments2 = post2.getComments();
-        assertNotNull(comments2);
-        assertEquals(1, comments2.size());
-        assertEquals("Nice explanation", comments2.get(0).getContent());
-
-        // 验证第二个用户的文章评论
-        User user2 = userList.get(1);
-        List<Post> posts2 = user2.getPosts();
-        assertNotNull(posts2);
-        assertEquals(2, posts2.size());
-
-        Post post3 = posts2.get(0);
-        List<Comment> comments3 = post3.getComments();
-        assertNotNull(comments3);
-        assertEquals(1, comments3.size());
-        assertEquals("Looking forward to more", comments3.get(0).getContent());
-    }
 }
