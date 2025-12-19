c191b2234f425ec5b1f38bd5b8fa918d682525a6
luyunfeng <biiiiiigmonster@gmail.com>
2025-12-19 11:37:15 +0800

    feat: WIP

--- jmodel-router/src/test/java/com/github/biiiiiigmonster/router/TestApplication.java
+++ jmodel-router/src/test/java/com/github/biiiiiigmonster/router/TestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.github.biiiiiigmonster.router.mapper")
-@ComponentScan(basePackages = {"com.github.biiiiiigmonster.router", "com.github.biiiiiigmonster.relation", "com.github.biiiiiigmonster.driver.mybatisplus"})
+@ComponentScan(basePackages = {"com.github.biiiiiigmonster"})
 public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
