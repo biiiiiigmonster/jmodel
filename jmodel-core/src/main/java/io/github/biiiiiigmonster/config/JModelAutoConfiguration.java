4b9f3bf017db13fc7c4716deabcdec9efa86b951
biiiiiigmonster <603707288@qq.com>
2026-03-23 15:19:12 +0800

    WIP

---
+++ jmodel-core/src/main/java/io/github/biiiiiigmonster/config/JModelAutoConfiguration.java
@@ -0,0 +1,11 @@
+package io.github.biiiiiigmonster.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "io.github.biiiiiigmonster")
+@EnableConfigurationProperties(CoreProperties.class)
+public class JModelAutoConfiguration {
+}
